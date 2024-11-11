a001abbe91ce2623deb8bdb9743d806893743cad
Jin Zhang <JinLZhang278@hotmail.com>
2024-11-11 13:26:20 -0500

    Routing config

--- my-admin-dashboard/src/app/app-routing.module.ts
+++ my-admin-dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './containers/default-layout/default-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      { path: 'dashboard', loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule) },
+      // Add more routes here
+    ]
+  },
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
